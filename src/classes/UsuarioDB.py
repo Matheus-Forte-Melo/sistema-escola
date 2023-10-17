bd517527eaa61e102fdd01e19756d8ca9349a7ea
Melao12125 <MatheusForte12125@gmail.com>
2023-10-17 16:08:08 -0300

    update

--- src/classes/UsuarioDB.py
+++ src/classes/UsuarioDB.py
@@ -2,7 +2,11 @@ import mysql.connector
 # Esta é a classe principal de todos os usuários. Todas as 3 categorias de usuário no sistema herdam as funcionalidade desta classe
 
 class UsuarioDB:
-    
+    def __init__(self, atributo) -> None:
+        self.atributo = atributo
+        self.__atributo_protegido = "Olá"
+        
+
     def _realizarLogin(self): 
         pass
 
@@ -19,5 +23,14 @@ class UsuarioDB:
         if self._conn is not None:
             self._conn.close()
             self._cursor.close()
+
+    @classmethod
+    def _instanciar_da_tupla(cls, tupla):
+        pass
             
 
+user = UsuarioDB("Teste")
+print(user._UsuarioDB__atributo_protegido)
+# Name mangling, como q eu não sabia dessa porra ainda, entao parta tornar realmente privado
+# Precisamos definir um setter (para restringir de fato) e um getter (para printar) nog
+#
